a64022950d2cd07081407f41fca8e363805c722b
Teja Sophista <tejanium@yahoo.com>
2022-03-22 15:38:32 +0000

    Fix `ActiveRecord::QueryMethods#in_order_of` when passing an out-of-range Integer.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -456,12 +456,16 @@ module ActiveRecord
       references = column_references([column])
       self.references_values |= references unless references.empty?
 
-      values = values.map { |value| type_caster.type_cast_for_database(column, value) }
+      if type_for_attribute(column).is_a?(ActiveRecord::Enum::EnumType)
+        values = values.map { |value| type_caster.type_cast_for_database(column, value) }
+      end
+
       arel_column = column.is_a?(Symbol) ? order_column(column.to_s) : column
+      where_condition = column.is_a?(Symbol) ? { column => values } : arel_column.in(values)
 
       spawn
         .order!(connection.field_ordered_value(arel_column, values))
-        .where!(arel_column.in(values))
+        .where!(where_condition)
     end
 
     # Replaces any existing order defined on the relation with the specified order.
