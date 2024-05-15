rails/rails

Add Capybara assertion support to Controller, Integration, Mailer, and View tests
This commit introduces framework-specific configuration values:

* `config.action_controller.assertions` for `ActionController::TestCase`
* `config.action_dispatch.assertions` for `ActionDispatch::IntegrationTest`
* `config.action_mailer.assertions` for `ActionMailer::TestCase`
* `config.action_view.assertions` for `ActionView::TestCase`

When set to `:capybara`, those tests include framework-scoped
`CapybaraAssertions` modules that transitively include
`Capybara::Minitest::Assertions`, and define the required `#page` method
to parse the HTML into a `Capybara::Node::Simple` instance.

When set to `:rails_dom_testing`, those tests include a framework-scoped
`RailsDomTestingAssetrions` module to preserve the existing behavior.

They all default to `:rails_dom_testing`.

actionmailer/test/capybara_assertions_test.rb
@@ -1,0 +1,77 @@
+# frozen_string_literal: true
+
+require "abstract_unit"
+
+class ActionMailer::CapybaraAssertionsTest < ActionMailer::TestCase
+  class TestMailer < ActionMailer::Base
+    def test(options)
+      text, html = options.values_at(:text, :html)
+
+      mail subject: "Test e-mail", from: "test@test.host", to: "test <test@test.host>" do |format|
+        if text.present?
+          format.text { render plain: text }
+        end
+        if html.present?
+          format.html { render plain: html }
+        end
+      end
+    end
+  end
+
+  include ActionView::CapybaraAssertions
+
+  tests TestMailer
+
+  test "assertions from last HTML delivery" do
+    TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>").deliver_now
+
+    within_html_part do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from HTML instance" do
+    mail = TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>", text: "ignored")
+
+    within_html_part mail do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from last multi-part delivery" do
+    TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>", text: "ignored").deliver_now
+
+    within_html_part do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from multi-part instance" do
+    mail = TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>")
+
+    within_html_part mail do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "fails when no HTML part" do
+    mail = TestMailer.test(text: "ignored")
+
+    assert_raises ArgumentError, match: "no HTML part in #{mail.inspect}" do
+      within_html_part mail do
+      end
+    end
+  end
+end

shunmugadigialert/ruby6

actionmailer/test/capybara_assertions_test.rb
@@ -1,0 +1,77 @@
+# frozen_string_literal: true
+
+require "abstract_unit"
+
+class ActionMailer::CapybaraAssertionsTest < ActionMailer::TestCase
+  class TestMailer < ActionMailer::Base
+    def test(options)
+      text, html = options.values_at(:text, :html)
+
+      mail subject: "Test e-mail", from: "test@test.host", to: "test <test@test.host>" do |format|
+        if text.present?
+          format.text { render plain: text }
+        end
+        if html.present?
+          format.html { render plain: html }
+        end
+      end
+    end
+  end
+
+  include ActionView::CapybaraAssertions
+
+  tests TestMailer
+
+  test "assertions from last HTML delivery" do
+    TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>").deliver_now
+
+    assert_html_part do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from HTML instance" do
+    mail = TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>", text: "ignored")
+
+    assert_html_part mail do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from last multi-part delivery" do
+    TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>", text: "ignored").deliver_now
+
+    assert_html_part do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "assertions from multi-part instance" do
+    mail = TestMailer.test(html: "<div><p>foo</p><p>bar</p></div>")
+
+    assert_html_part mail do
+      assert_css "div" do |div|
+        assert_css div, "p", text: "foo"
+        assert_css div, "p", text: "bar"
+      end
+    end
+  end
+
+  test "fails when no HTML part" do
+    mail = TestMailer.test(text: "ignored")
+
+    assert_raises Minitest::Assertion, match: "expected part matching text/html in #{mail.inspect}" do
+      assert_html_part mail do
+      end
+    end
+  end
+end